00278456d65bba743210673a0952b90f5445e0d3
joao candido <joan.jb56@gmail.com>
2022-09-08 20:25:18 -0300

    modifiquei

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'biblioteca-virtual';
-}
+
+
+
+ cadastrar(){
+   
+   const formulario = document.querySelector("#form-control")
+   console.log(formulario)
+ }}
